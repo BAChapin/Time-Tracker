4f6ab06ede5581f69211afa2dbe8880bbbb23ece
Brett Chapin <brett.chapin89@gmail.com>
2022-11-06 19:50:54 -0500

    Added the Xcode Project

---
+++ TimeTracker/TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Brett Chapin on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
